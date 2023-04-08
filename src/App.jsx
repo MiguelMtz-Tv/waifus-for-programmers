afc3f6cad6722831a00f326018fc18af4082f13c
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-04-07 23:46:20 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Content from "./layout/Content";
+import Sidebar from "./layout/Sidebar";
+
+function App(){
+    return(
+        <div>
+            <Sidebar/>
+            <Content/> 
+        </div>
+    )
+}
+
+export default App;
