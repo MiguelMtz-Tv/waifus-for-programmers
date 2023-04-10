01929f3ab291c32250f788bc3732465cf5d6889b
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-04-09 22:22:25 -0600

    sidebar list

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 
 function Content(){
     return(
-    <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
+    <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
         
         </div>
     </div>
