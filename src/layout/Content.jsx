6712e32330e4b5b96b94762b662630ad0dea1bef
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-04-13 08:50:51 -0600

    added: content styles

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -2,14 +2,8 @@ import { useState, useEffect } from "react";
 
 function Content({lang}){
     const [imgs, setImgs] = useState([]);
-    /* const [dataLang, setLang] = useState('Javascript'); */
+
     const WAIFUS_ENDPOINT = 'https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents/'
-    
-    /* const fetchImages = () => {
-        fetch(WAIFUS_ENDPOINT+dataLang)
-        .then(res => res.json())
-        .then(data => setImgs(data))
-    } */
 
     useEffect(() => {
         fetch(WAIFUS_ENDPOINT+lang)
@@ -19,12 +13,13 @@ function Content({lang}){
 
     return(
     <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
-        {/* <button onClick={show}>show me</button> */}
+        <div className="flex justify-center flex-wrap p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700 mt-14">
             {
                 imgs.map((img) => {
                     return(
-                        <img src={img.download_url}/>
+                        <div className="mx-14 mt-14">
+                            <img src={img.download_url} className="rounded-sm w-64"/>
+                        </div>
                     )
                 })
             }
